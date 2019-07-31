bb6184c3395674f786e777e94887b8fb7f70ea9c
Dycrno <53318614+Dycrno@users.noreply.github.com>
2019-07-30 20:44:30 -0400

    Update myprogram.py

--- myprogram.py
+++ myprogram.py
@@ -1,2 +1,5 @@
-print("Learning Tracker")
-
+import argparse
+parser = argparse.ArgumentParser(description='Request from a url.')
+parser.add_argument('url', metavar='N', type=string, nargs='+', help='the url you wish to request from')
+args = parser.parse_args()
+print args.url
