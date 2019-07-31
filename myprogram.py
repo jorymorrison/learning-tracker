46ecbe6404e4e9cf0da441dda590f67bed9a1402
Dycrno <53318614+Dycrno@users.noreply.github.com>
2019-07-31 11:55:24 -0400

    Update myprogram.py

--- myprogram.py
+++ myprogram.py
@@ -1,5 +1,24 @@
-import argparse
-parser = argparse.ArgumentParser(description='Request from a url.')
-parser.add_argument('url', metavar='N', type=str, nargs='+', help='the url you wish to request from')
-args = parser.parse_args()
-print args.url
+import configparser
+
+Config = configparser.ConfigParser()
+
+file = input("\nEnter the file location: ")
+Config.read(file)
+
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
+site = input("\nEnter the site you want info from: ")
+Name = ConfigSectionMap(site)['name']
+Url = ConfigSectionMap(site)['url']
+Api = ConfigSectionMap(site)['api']
