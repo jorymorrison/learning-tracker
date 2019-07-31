5bd8b3f5753d46b445d8cd60367a0778b6a5ac9e
Dycrno <53318614+Dycrno@users.noreply.github.com>
2019-07-30 20:50:01 -0400

    Update myprogram.py

--- myprogram.py
+++ myprogram.py
@@ -1,5 +1,5 @@
 import argparse
 parser = argparse.ArgumentParser(description='Request from a url.')
-parser.add_argument('url', metavar='N', type=string, nargs='+', help='the url you wish to request from')
+parser.add_argument('url', metavar='N', type=String, nargs='+', help='the url you wish to request from')
 args = parser.parse_args()
 print args.url
