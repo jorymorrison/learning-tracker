a223eb3742909b72e52bc768b5d5a62a8439887f
Dycrno <zacharysiedlecki01@gmail.com>
2019-08-02 18:27:32 -0400

    Updated code

--- myprogram.py
+++ myprogram.py
@@ -11,13 +11,13 @@ def get_api_token(file):
         return r['results']
 
 def get_base64_encoded_header(client_id, client_secret):
-    auth_token = 'Basic' + base64.b64encode("{}:{}".format(client_id, client_secret).encode{}).decode('ascii')
+    auth_token = 'Basic' + base64.b64encode("{}:{}".format(client_id, client_secret).encode()).decode('ascii')
     headers = {'Authorization':auth_token}
     return headers
 
 def get_api_response(portal_name, portal_id, client_id, client_secret):
     url = 'https://' + portal_name + '.udemy.com/api-2.0/organizations/' + str(portal_id) + '/courses/list'
-    r = requests.request("GET, url, headers=get_base64_encoded_header(client_id, client_secret))
+    r = requests.request("GET", url, headers=get_base64_encoded_header(client_id, client_secret))
     return r
     
 Config = configparser.ConfigParser()
@@ -48,6 +48,6 @@ Org = ConfigSectionMap("udemy")['org id']
 
 print("Name of site: " + Name)
 print("Site URL: " + Url)
-print("Client id: " + Client)
-print("API key: " + Secret)
+print("Client id: " + client_id)
+print("API key: " + client_secret)
 print("Org id: " + Org)
